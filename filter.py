56aae76b94af169e6636dfb0f19fda4defec95b7
Patrick <baipenghan@gmail.com>
2013-04-10 21:56:06 -0700

    Reduce Morphy of Noun and Verb

--- filter.py
+++ filter.py
@@ -1,4 +1,6 @@
 import nltk
+from nltk.stem import WordNetLemmatizer
+
 
 one_hundred = ['the', 'of', 'and', 'a', 'to', 'in', 'is', 'you', 'that', 'it', 'he', 'was', 'for', 'on', 'are', 'as', 'with', 'his', 'they', 'I', 'at', 'be', 'this', 'have', 'from', 'or', 'had', 'by', 'but', 'not', 'what', 'were', 'we', 'when', 'your', 'can', 'said', 'there', 'an', 'which', 'she', 'do', 'how', 'their', 'if', 'will', 'up', 'other', 'about', 'out', 'then', 'them', 'these', 'so', 'her', 'would', 'him', 'into', 'has', 'no', 'could', 'my', 'than', 'been', 'who', 'its', 'down', 'did', 'may']
 one_hundred = set(one_hundred)
@@ -6,9 +8,24 @@ taggers = ['ABL', 'ABN', 'ABX', 'AP', 'CD', 'EX', 'FW', 'JJ', 'JJR', 'JJS', 'JJT
 taggers = set(taggers)
 punctuation = '.?!,#*():;\'\"'
 
+
+def add_dict(pair, dic, wnl):
+    if pair[1] in taggers and pair[0].lower() not in one_hundred and pair[0] not in punctuation:
+        if pair[1].startswith('V'):
+            word = wnl.lemmatize(pair[0], 'v').lower()
+        elif pair[1].startswith('N'):
+            word = wnl.lemmatize(pair[0]).lower()
+        else:
+            word = pair[0].lower()
+        if word in dic:
+            dic[word] += 1
+        else:
+            dic[word] = 1
+        
 def filter(fname):
     f = open(fname, 'r')
     
+    wnl = WordNetLemmatizer()
     final_tuple = []
     word_dic = dict()
     subject = ''
@@ -29,8 +46,7 @@ def filter(fname):
     subject_tokens = nltk.word_tokenize(subject)
     subject_tagged = nltk.pos_tag(subject_tokens)
     for pair in subject_tagged:
-        if pair[1] in taggers and pair[0].lower() not in one_hundred:
-            final_tuple.append(pair)
+        add_dict(pair, word_dic, wnl)
     
     
     #Parse Body
@@ -42,15 +58,10 @@ def filter(fname):
         tagged += nltk.pos_tag(tokens)
     
     for pair in tagged:
-        if pair[1] in taggers and pair[0].lower() not in one_hundred and pair[0] not in punctuation:
-            final_tuple.append(pair)
+        add_dict(pair, word_dic, wnl)
+                
     
-    for pair in final_tuple:
-        word = pair[0].lower()
-        if word in word_dic:
-            word_dic[word] += 1
-        else:
-            word_dic[word] = 1
+   
     '''
     Remove only appering once word in dictionary
     
@@ -61,6 +72,8 @@ def filter(fname):
     '''
     
     print word_dic
+    
+    
 
     
 filter('/Users/taurus/Documents/CPSC540/preschedule/1')
